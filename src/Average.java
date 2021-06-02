0be92903f36393c4f1a3636d166eb0f3e78aa45f
Fatima Ibrahim <FIbrahim@harriscomputer.com>
2021-06-02 16:58:17 +0500

    new comment in bugfix

--- src/Average.java
+++ src/Average.java
@@ -2,6 +2,7 @@
 public class Average {
 
 	public static void main(String[] args) {
+		//a change only in stage
 		double[] numArray = { 45.3, 67.5, -45.6, 20.34, 33.0};
         double sum = 0.0;
 
